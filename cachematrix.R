c128443b5c4cbd74f82a138d34c92f1519852677
awezcoursera <awez@yahoo.com>
2015-06-20 06:54:25 -0700

    checkin 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,9 @@
 ## Write a short comment describing this function
 ## this is a test comment to see if this works
 
+## creates a special vector with functions to get and set the inverse of a matrix
+
+
 makeCacheMatrix <- function(x = matrix()) {
 
         m <- NULL
@@ -21,7 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix, if the inverse
+## is already available it returns the cached value otherwise computes it
+## The special matrix was generated with a call to the makeCacheMatrix() function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
